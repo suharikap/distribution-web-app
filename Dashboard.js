9151e55cca86356f9dd05cfce3a3579f158cf627
suharikap <suharikap@gmail.com>
2015-08-27 22:28:56 +0530

    created charts for dash board

---
+++ Dashboard.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+
+$("#doughnutChart").drawDoughnutChart([
+  { 
+    title: "30% left",
+    value : 4822,  
+    color: "#abb3e2" 
+  },
+  { 
+    title: "70% Achieved",
+    value:  12339,   
+    color: "#3F51B5" 
+  }
+]);
+
+
+var ctx = document.getElementById("line-chart").getContext("2d");
+var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+};
+var options = {
+    maintainAspectRatio: false,
+    responsive: true
+};
+var myLineChart = new Chart(ctx).Line(data, options);
+
+
+
+
+var ctx = document.getElementById("bar-chart").getContext("2d");
+
+var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.5)",
+            strokeColor: "rgba(151,187,205,0.8)",
+            highlightFill: "rgba(151,187,205,0.75)",
+            highlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+};
+var options={}
+var myBarChart = new Chart(ctx).Bar(data, options);
+});
